374f64178f646ebb78596b16b2f9865a708dbedd
baluart <lucasbaluart@gmail.com>
2022-11-26 13:58:37 -0300

    classe user resource findById

--- src/main/java/br/com/api/apitestunitarios/resources/UserResources.java
+++ src/main/java/br/com/api/apitestunitarios/resources/UserResources.java
@@ -1,6 +1,7 @@
 package br.com.api.apitestunitarios.resources;
 
 import br.com.api.apitestunitarios.domain.User;
+import br.com.api.apitestunitarios.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResources {
 
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new User(1,"Lucas","lucasbaluart@gmail.com","123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
