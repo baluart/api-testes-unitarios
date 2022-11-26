d871e800a51b5082bd179a86c13ed924a8d41425
baluart <lucasbaluart@gmail.com>
2022-11-26 12:11:25 -0300

    projeto iniciado

---
+++ src/main/java/br/com/api/apitestunitarios/ApitestunitariosApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.apitestunitarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitestunitariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApitestunitariosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/api/apitestunitarios/ApitestunitariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.apitestunitarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitestunitariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
