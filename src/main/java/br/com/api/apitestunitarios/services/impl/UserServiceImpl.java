16f4155e0ddaef6dac1881643378afd94901c964
baluart <lucasbaluart@gmail.com>
2022-11-27 11:22:13 -0300

    Tratamento de erro para objeto nao encontrado.

--- src/main/java/br/com/api/apitestunitarios/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/api/apitestunitarios/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.api.apitestunitarios.services.impl;
 import br.com.api.apitestunitarios.domain.User;
 import br.com.api.apitestunitarios.repositories.UserRepository;
 import br.com.api.apitestunitarios.services.UserService;
+import br.com.api.apitestunitarios.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não Encontrado"));
     }
 }
