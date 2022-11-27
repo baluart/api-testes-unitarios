afb9484923cc92b2cd1df1e05cd0284f59fb85fd
baluart <lucasbaluart@gmail.com>
2022-11-27 15:34:45 -0300

    excecao para email na atualização

--- src/main/java/br/com/api/apitestunitarios/resources/UserResources.java
+++ src/main/java/br/com/api/apitestunitarios/resources/UserResources.java
@@ -43,4 +43,11 @@ public class UserResources {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO obj){
+    obj.setId(id);
+    User newObj = service.update(obj);
+    return ResponseEntity.ok().body(mapper.map(newObj, UserDTO.class));
+    }
 }
