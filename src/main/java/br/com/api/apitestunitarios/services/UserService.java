cad0487c6e18d06b0f829499b8774fba016930c9
baluart <lucasbaluart@gmail.com>
2022-11-27 12:16:25 -0300

    Endpoint FindAll para User

--- src/main/java/br/com/api/apitestunitarios/services/UserService.java
+++ src/main/java/br/com/api/apitestunitarios/services/UserService.java
@@ -2,6 +2,10 @@ package br.com.api.apitestunitarios.services;
 
 import br.com.api.apitestunitarios.domain.User;
 
+import java.util.List;
+
 public interface UserService {
     User findById(Integer id);
+    List<User>findAll();
+
 }
