54d153e7b2b28190e047b8d0be983e89df8e915f
baluart <lucasbaluart@gmail.com>
2022-12-06 10:18:00 -0300

    testes para a classe User service metodo update

--- src/test/java/br/com/api/apitestunitarios/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/api/apitestunitarios/services/impl/UserServiceImplTest.java
@@ -119,7 +119,18 @@ class UserServiceImplTest {
     }
 
     @Test
-    void update() {
+    void whenUpdateThenReturnSuccess() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.update(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NOME, response.getNome());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
+
     }
 
     @Test
