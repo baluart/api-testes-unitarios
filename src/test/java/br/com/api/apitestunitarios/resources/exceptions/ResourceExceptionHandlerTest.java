28f9a6dbdcf3d27be7f76e856a148a46d908d2c8
baluart <lucasbaluart@gmail.com>
2022-12-06 15:59:50 -0300

    teste para manipulador de excecoes metodo data integrity

--- src/test/java/br/com/api/apitestunitarios/resources/exceptions/ResourceExceptionHandlerTest.java
+++ src/test/java/br/com/api/apitestunitarios/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package br.com.api.apitestunitarios.resources.exceptions;
 
+import br.com.api.apitestunitarios.services.exceptions.DataIntegratyViolationException;
 import br.com.api.apitestunitarios.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,12 +12,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ResourceExceptionHandlerTest {
 
     public static final String OBJETO_NAO_ENCONTRADO = "Objeto nao encontrado";
+    public static final String EMAIL_JA_CADASTRADO = "Email ja cadastrado";
     @InjectMocks
     private ResourceExceptionHandler exceptionHandler;
 
@@ -42,5 +46,18 @@ class ResourceExceptionHandlerTest {
 
     @Test
     void dateIntegratyViolationException() {
+        ResponseEntity<StandardError> response = exceptionHandler
+                .dateIntegratyViolationException(
+                        new DataIntegratyViolationException(EMAIL_JA_CADASTRADO),
+                        new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(EMAIL_JA_CADASTRADO,response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
+        assertNotEquals("/user/2", response.getBody().getPath());
+        assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
     }
 }
